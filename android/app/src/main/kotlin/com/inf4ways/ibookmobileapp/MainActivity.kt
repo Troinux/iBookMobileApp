20554195547f0ac9d0e3484a4fd8d889de3c8535
Troinux <201606327@keyaka.ul.ac.za>
2018-07-14 19:18:45 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/inf4ways/ibookmobileapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.inf4ways.ibookmobileapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
